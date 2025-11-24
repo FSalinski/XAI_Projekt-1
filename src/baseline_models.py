d7ceab1629c96f542a620882fda64490ddcb8724
Bartosz Pokora <bartosz.pokora@gmail.com>
2025-11-24 09:55:27 +0100

    fix baseline models

--- src/baseline_models.py
+++ src/baseline_models.py
@@ -1,20 +1,21 @@
 '''
 Script for training 4 baseline models:
  - Logistic Regression on full feature set
- - Logistic Regression on reduced feature set (after removing correlated features)
+ - Logistic Regression on reduced feature set (after removing pairwise correlated features)
  - Random Forest on full feature set
- - Random Forest on reduced feature set (after removing correlated features)
+ - Random Forest on reduced feature set (after removing pairwise correlated features)
 '''
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from utils import remove_all_correlated_features, evaluate_baseline_model, save_model
+from utils import remove_all_correlated_features, evaluate_model, save_model
 from data_processing import baseline_data_processing_pipeline
 import logging
 
 RANDOM_STATE = 2137
+CORR_THRESHOLD = 0.75
 
 def main():
     # Set up logging
@@ -39,34 +40,40 @@ def main():
     X_test_processed = preprocessor.transform(X_test)
     logging.info("Data processing pipeline applied")
 
+    # Logistic REgression and Random Forest models
+    lr = LogisticRegression(random_state=RANDOM_STATE, class_weight='balanced')
+    rf = RandomForestClassifier(random_state=RANDOM_STATE, class_weight='balanced')
+
+    # Create dataset with reduced features (removing pairwise correlated features)
+    X_train_reduced = remove_all_correlated_features(pd.DataFrame(X_train_processed), threshold=CORR_THRESHOLD)
+    X_test_reduced = pd.DataFrame(X_test_processed)[X_train_reduced.columns]
+
     # Logistic Regression on full feature set
-    log_reg_full = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000)
+    log_reg_full = lr
     log_reg_full.fit(X_train_processed, y_train)
-    acc_full, roc_auc_full = evaluate_baseline_model(log_reg_full, X_test_processed, y_test)
-    logging.info(f"Logistic Regression (full features) - Accuracy: {acc_full}, ROC AUC: {roc_auc_full}")
+    rec_full_lr, roc_auc_full_lr = evaluate_model(log_reg_full, X_test_processed, y_test)
+    logging.info(f"Logistic Regression (full features) - Recall: {rec_full_lr}, ROC AUC: {roc_auc_full_lr}")
     # save_model(log_reg_full, './models/logistic_regression_full.pkl')
 
     # Logistic Regression on reduced feature set
-    X_train_reduced = remove_all_correlated_features(pd.DataFrame(X_train_processed), threshold=0.75)
-    X_test_reduced = X_test_processed[:, X_train_reduced.columns]
-    log_reg_reduced = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000)
+    log_reg_reduced = lr
     log_reg_reduced.fit(X_train_reduced, y_train)
-    acc_reduced, roc_auc_reduced = evaluate_baseline_model(log_reg_reduced, X_test_reduced, y_test)
-    logging.info(f"Logistic Regression (reduced features) - Accuracy: {acc_reduced}, ROC AUC: {roc_auc_reduced}")
+    rec_reduced_lr, roc_auc_reduced_lr = evaluate_model(log_reg_reduced, X_test_reduced, y_test)
+    logging.info(f"Logistic Regression (reduced features) - Recall: {rec_reduced_lr}, ROC AUC: {roc_auc_reduced_lr}")
     # save_model(log_reg_reduced, './models/logistic_regression_reduced.pkl')
 
     # Random Forest on full feature set
-    rf_full = RandomForestClassifier(random_state=RANDOM_STATE)
+    rf_full = rf
     rf_full.fit(X_train_processed, y_train)
-    acc_rf_full, roc_auc_rf_full = evaluate_baseline_model(rf_full, X_test_processed, y_test)
-    logging.info(f"Random Forest (full features) - Accuracy: {acc_rf_full}, ROC AUC: {roc_auc_rf_full}")
+    rec_full_rf, roc_auc_full_rf = evaluate_model(rf_full, X_test_processed, y_test)
+    logging.info(f"Random Forest (full features) - Recall: {rec_full_rf}, ROC AUC: {roc_auc_full_rf}")
     # save_model(rf_full, './models/random_forest_full.pkl')
 
     # Random Forest on reduced feature set
-    rf_reduced = RandomForestClassifier(random_state=RANDOM_STATE)
+    rf_reduced = rf
     rf_reduced.fit(X_train_reduced, y_train)
-    acc_rf_reduced, roc_auc_rf_reduced = evaluate_baseline_model(rf_reduced, X_test_reduced, y_test)
-    logging.info(f"Random Forest (reduced features) - Accuracy: {acc_rf_reduced}, ROC AUC: {roc_auc_rf_reduced}")
+    rec_reduced_rf, roc_auc_reduced_rf = evaluate_model(rf_reduced, X_test_reduced, y_test)
+    logging.info(f"Random Forest (reduced features) - Recall: {rec_reduced_rf}, ROC AUC: {roc_auc_reduced_rf}")
     # save_model(rf_reduced, './models/random_forest_reduced.pkl')
     logging.info("=" * 50)
 
